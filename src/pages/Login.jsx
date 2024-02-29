b0588d42768ffdb7dd888934d083f169053ac24a
witthayadevkub <tigermixdev@gmail.com>
2024-03-01 05:04:57 +0700

    add feature button guest and loading

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,8 +5,9 @@ import { useAuthContext } from '../main';
 
 
 const Login = () => {
-const { authUser, setAuthUser } = useAuthContext()
+    const { authUser, setAuthUser } = useAuthContext()
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false)
     const [form, setForm] = useState({
         email: "",
         password: "",
@@ -21,21 +22,41 @@ const { authUser, setAuthUser } = useAuthContext()
         e.preventDefault()
         try {
             if (form.email !== "" || form.password !== "") {
+                setLoading(true)
                 const data = await login(form)
                 setAuthUser(data)
                 navigate('/')
                 window.location.reload()
             }
+            
+                setLoading(false)
+         
         } catch (err) {
             console.log(err);
         }
     };
 
+    const handleGuest = async () => {
+        try {
+            setLoading(true)
+            const guestUser = { email: "user@gmail.com", password: "000000" }
+            const data = await login(guestUser)
+            setAuthUser(data)
+            navigate('/')
+            window.location.reload()
+            setLoading(false)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    
 
     return (
         <form onSubmit={handleSubmit} className='relative max-w-[400px] shadow bg-white p-4 flex flex-col gap-2 m-[0_auto] rounded-lg '>
             <div className="">
                 <h2 className='text-xl font-bold text-center'>LogIn</h2>
+                {loading && <span className="loading loading-dots loading-lg"></span>}
             </div>
 
             <label className="input input-bordered flex items-center gap-2">
@@ -46,14 +67,16 @@ const { authUser, setAuthUser } = useAuthContext()
             <label className="input input-bordered flex items-center gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
 
-                <input type="password" className="grow" placeholder="Password"  maxLength={6} name='password' onChange={handleForm} value={form.password} />
+                <input type="password" className="grow" placeholder="Password" maxLength={6} name='password' onChange={handleForm} value={form.password} />
                 <p className="text-gray-500 font-semibold">{form.password.length}/6</p>
             </label>
 
             <Link to='/signup' className="underline hover:btn-link text-sm">Go Signup</Link>
+            <div className="flex gap-1">
+                <button type='submit' className="flex-1 btn btn-outline btn-accent">Login</button>
+                <button onClick={handleGuest} className=" btn btn-outline btn-accent">Guest</button>
+            </div>
 
-            <button type='submit' className="btn btn-outline btn-accent">Login</button>
-           
         </form>
     )
 }
